3ec458df9896f1aae0a9e6ceb0d36525319e84e7
Sergushka <39465148+Sergushka@users.noreply.github.com>
2023-04-03 10:08:34 +0000

    Deploying to gh-pages from @ Sergushka/tictactoe_wasm@fd26830f708a77becdb13b4356b252700b8df01b 🚀

---
+++ tictactoe_web_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function getState(a: number): void;
+export function makeTurn(a: number): void;
+export function isGameOver(): number;
+export function getLastPlayed(a: number): void;
+export function restart(): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
